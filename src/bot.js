07153f406474675b11cff699a2f859419ab557ba
Anatole Dufour <konixy.p@gmail.com>
2022-11-29 17:10:19 +0100

    Update

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,4 @@
+// @ts-ignore
 const Discord = require('discord.js');
 const client = new Discord.Client({
     intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_BANS, Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Discord.Intents.FLAGS.GUILD_INTEGRATIONS, Discord.Intents.FLAGS.GUILD_WEBHOOKS, Discord.Intents.FLAGS.GUILD_INVITES, Discord.Intents.FLAGS.GUILD_VOICE_STATES, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING, Discord.Intents.FLAGS.GUILD_PRESENCES],
@@ -15,7 +16,7 @@ const passwordGenerator = require('generate-password')
 const database = require('./database')
 
 function sendEmbed(message, embedTitle, embedDescription, embedColorParam) {
-    if(!message || !embedTitle && !embedDescription) return console.log('[sendEmbed function ERROR] invalid arguments')
+    if(!message || !embedTitle && !embedDescription) return console.log('[ERROR] sendEmbed() function recieved invalid arguments')
     const embed = new Discord.MessageEmbed()
     if(embedTitle) embed.setTitle(embedTitle);
     if(embedColorParam) embed.setColor(embedColorParam); else embed.setColor(embedColor);
