2f58f5e201ae3772a14b07d341c25a538557ad59
Konixy <konixy.p@gmail.com>
2023-06-24 17:41:24 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
+import { config } from "dotenv";
+
+config();
+
+async function main() {
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: Number(process.env.PORT) },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+}
+
+main();
