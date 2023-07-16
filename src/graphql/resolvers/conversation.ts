bd8c52cbc574ab0ecf3dbdb0b6922c9cf3891ef3
Konixy <konixy.p@gmail.com>
2023-07-17 01:44:17 +0200

    Update

--- src/graphql/resolvers/conversation.ts
+++ src/graphql/resolvers/conversation.ts
@@ -1,5 +1,6 @@
 import { ApolloError } from "apollo-server-core";
 import { GraphQLContext } from "../../lib/types";
+import { Prisma } from "@prisma/client";
 
 const resolvers = {
   Mutation: {
@@ -7,13 +8,71 @@ const resolvers = {
       _: any,
       { participantsIds }: { participantsIds: string[] },
       { session, prisma }: GraphQLContext
-    ) => {
+    ): Promise<{ conversationId: string }> => {
       console.log(participantsIds);
 
-      throw new ApolloError("Function not implemented");
+      if (!session.user) {
+        throw new ApolloError("Not authorized.");
+      }
+
+      const {
+        user: { id: userId },
+      } = session;
+
+      try {
+        // if a conversation exists with the following participants, just return the conversation id of the existing conversation
+
+        const conversation = await prisma.conversation.create({
+          data: {
+            id: String(Date.now()),
+            participants: {
+              createMany: {
+                data: participantsIds.map((id) => ({
+                  userId: id,
+                })),
+              },
+            },
+          },
+          include: conversationPopulated,
+        });
+
+        return {
+          conversationId: conversation.id,
+        };
+      } catch (err) {
+        console.log("createConversation ERROR", err);
+        throw new ApolloError("Error creating conversation");
+      }
     },
   },
   // Subscription: {},
 };
+
+export const participantPopulated =
+  Prisma.validator<Prisma.ConversationParticipantInclude>()({
+    user: {
+      select: {
+        id: true,
+        username: true,
+      },
+    },
+  });
+
+export const conversationPopulated =
+  Prisma.validator<Prisma.ConversationInclude>()({
+    participants: {
+      include: participantPopulated,
+    },
+    latestMessage: {
+      include: {
+        sender: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+      },
+    },
+  });
 
 export default resolvers;
